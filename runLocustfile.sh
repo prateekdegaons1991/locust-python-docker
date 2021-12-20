115ec9d41bfc3d95f2835eeaca60dee1a3ca22fb
pratikdegaon <prateekdegaons1991@outlook.com>
2021-12-20 18:12:08 +0530

    shell script corrected

--- runLocustfile.sh
+++ runLocustfile.sh
@@ -4,12 +4,6 @@
 #
 #####################################################################
 
-<<<<<<< HEAD
-locust -f $LOCUST_FILE --headless -u 100 -r 10 -H https://prateeks-blog.herokuapp.com --run-time 1min
-=======
-
-
 #locust -f $LOCUST_FILE --headless -u 100 -r 10 -H https://prateeks-blog.herokuapp.com --run-time 1min
 
 locust --headless --run-time $RUN_TIME -f $LOAD_FILE --users $USERS --spawn-rate $SPAWN_RATE -H $HOST
->>>>>>> 76e3049 (yaml added with changes in environ)
